4611c516ee109f9f453bb659f9482f1c36ef12ca
Patrick Dawson <patrick.dawson@zwick.de>
2016-11-22 08:14:54 +0100

    Added callback with argument.

--- chapter1_3/callback.cpp
+++ chapter1_3/callback.cpp
@@ -11,8 +11,20 @@ void CallThis(const FunctionCallbackInfo<Value>& args) {
     return;
 }
 
+void CallThis_WithThis(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    Local<Function> cb = Local<Function>::Cast(args[0]);
+    Local<Value> argv[1] = {args[1]};
+
+    cb->Call(Null(isolate), 1, argv);
+
+    return;
+}
+
  void init(Local<Object> exports) {
     NODE_SET_METHOD(exports, "callthis", CallThis);
+    NODE_SET_METHOD(exports, "callthis_withthis", CallThis_WithThis);
  }
 
  NODE_MODULE(callback, init)
