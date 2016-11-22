ebc4d17fd824ea4a997de6ebd6b195a5ab5f372c
Patrick Dawson <patrick.dawson@zwick.de>
2016-11-22 11:23:13 +0100

    Added integer and boolean

--- chapter2_2/addon.cpp
+++ chapter2_2/addon.cpp
@@ -6,6 +6,12 @@ using namespace v8;
 void PassNumber(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
+    // This would catch anything that can't be a Number
+    /*if ( !args[0]->IsNumber()) {
+        args.GetReturnValue().Set(Number::New(isolate, -1));
+        return;
+    }*/
+
     Local<Number> target = args[0]->ToNumber();
     double value = target->NumberValue();
 
@@ -15,8 +21,30 @@ void PassNumber(const FunctionCallbackInfo<Value>& args) {
     args.GetReturnValue().Set(retval);
 }
 
+void PassInteger(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    Local<Int32> target = args[0]->ToInt32();
+    int value = target->NumberValue();
+
+    Local<Number> retval = Number::New(isolate, value);
+    args.GetReturnValue().Set(retval);
+}
+
+void PassBoolean(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    Local<Boolean> target = args[0]->ToBoolean();
+    bool value = target->BooleanValue();
+
+    Local<Number> retval = Number::New(isolate, value);
+    args.GetReturnValue().Set(retval);
+}
+
  void init(Local<Object> exports) {
     NODE_SET_METHOD(exports, "passNumber", PassNumber);
+    NODE_SET_METHOD(exports, "passInteger", PassInteger);
+    NODE_SET_METHOD(exports, "passBoolean", PassBoolean);
  }
 
  NODE_MODULE(addon, init)
--- chapter2_2/app.js
+++ chapter2_2/app.js
@@ -1,3 +1,20 @@
 const addon = require('./build/Release/addon');
 
-console.log(addon.passNumber(1));
+console.log("Numbers");
+console.log("-------");
+console.log(addon.passNumber("2"));
+console.log(addon.passNumber(2));
+
+console.log("\nInteger");
+console.log("-------");
+console.log(addon.passInteger(2));
+console.log(addon.passInteger(3.7));
+
+console.log("\nBool");
+console.log("-------");
+console.log(addon.passBoolean(true));
+console.log(addon.passBoolean("some"));
+console.log(addon.passBoolean(2));
+console.log(addon.passBoolean(false));
+console.log(addon.passBoolean(null));
+console.log(addon.passBoolean(undefined));
