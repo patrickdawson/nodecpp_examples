eff5ef8bd06bd9c2f166878919cdfbf3fc75e97b
Dawson, Patrick <patrick.dawson79@gmail.com>
2017-05-08 08:00:26 +0200

    Finished chapter 5.

--- chapter5_1/app.js
+++ chapter5_1/app.js
@@ -1,5 +1,8 @@
 "use strict";
-const addon = require('./cpp/build/Release/polynomial');
+const Polynomial = require('./cpp/build/Release/polynomial').Polynomial;
 
-const poly = new addon.Polynomial(1, 3, 2);
-console.log(poly);
+var poly = new Polynomial(1, 3, 2);
+console.log(poly.at(4)) // prints 30
+poly.c = 0;
+console.log(poly.at(4)) // prints 28
+console.log(poly); // Polynomial {c:0}
--- chapter5_1/cpp/poly_node.cpp
+++ chapter5_1/cpp/poly_node.cpp
@@ -1,5 +1,7 @@
 #include <node.h>
 #include <node_object_wrap.h>
+#include <math.h>
+#include <string>
 
 class WrappedPoly : public node::ObjectWrap
 {
@@ -11,6 +13,11 @@ private:
     ~WrappedPoly();
 
     static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+    static void At(const v8::FunctionCallbackInfo<v8::Value>& args);
+    static void Roots(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+    static void GetCoeff(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info);
+    static void SetCoeff(v8::Local<v8::String> property, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info);
 
     static v8::Persistent<v8::Function> constructor;
     double a_;
@@ -31,8 +38,14 @@ void WrappedPoly::Init(v8::Local<v8::Object> exports)
     tpl->SetClassName(v8::String::NewFromUtf8(isolate, "Polynomial"));
     tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-    constructor.Reset(isolate, tpl->GetFunction());
+    NODE_SET_PROTOTYPE_METHOD(tpl, "at", At);
+    NODE_SET_PROTOTYPE_METHOD(tpl, "roots", Roots);
 
+    tpl->InstanceTemplate()->SetAccessor(v8::String::NewFromUtf8(isolate, "a"), GetCoeff, SetCoeff);
+    tpl->InstanceTemplate()->SetAccessor(v8::String::NewFromUtf8(isolate, "b"), GetCoeff, SetCoeff);
+    tpl->InstanceTemplate()->SetAccessor(v8::String::NewFromUtf8(isolate, "c"), GetCoeff, SetCoeff);
+
+    constructor.Reset(isolate, tpl->GetFunction());
     exports->Set(v8::String::NewFromUtf8(isolate, "Polynomial"), tpl->GetFunction());
 }
 
@@ -64,6 +77,78 @@ void WrappedPoly::New(const v8::FunctionCallbackInfo<v8::Value>& args)
         v8::Local<v8::Value> argv[argc] = { args[0], args[1], args[2] };
         v8::Local<v8::Function> cons = v8::Local<v8::Function>::New(isolate, constructor);
         args.GetReturnValue().Set(cons->NewInstance(argc, argv));
+    }
+}
+
+void WrappedPoly::At(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+    v8::Isolate* isolate = args.GetIsolate();
+    double x = args[0]->IsUndefined() ? 0 : args[0]->NumberValue();
+    WrappedPoly* poly = ObjectWrap::Unwrap<WrappedPoly>(args.Holder());
+
+    double results = x * x * poly->a_ + x * poly->b_ + poly->c_;
+    args.GetReturnValue().Set(v8::Number::New(isolate, results));
+}
+
+void WrappedPoly::Roots(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+    v8::Isolate* isolate = args.GetIsolate();
+    WrappedPoly* poly = ObjectWrap::Unwrap<WrappedPoly>(args.Holder());
+
+    v8::Local<v8::Array> roots = v8::Array::New(isolate);
+    double desc = poly->b_ * poly->b_ - (4 * poly->a_ * poly->c_);
+    if (desc >= 0) {
+        double r = (-poly->b_ + sqrt(desc)) / (2 * poly->a_);
+        roots->Set(0, v8::Number::New(isolate, r));
+        if (desc > 0) {
+            r = (-poly->b_ - sqrt(desc)) / (2 * poly->a_);
+            roots->Set(1, v8::Number::New(isolate, r));
+        }
+    }
+    args.GetReturnValue().Set(roots);
+}
+
+void WrappedPoly::GetCoeff(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    v8::Isolate* isolate = info.GetIsolate();
+    WrappedPoly* poly = ObjectWrap::Unwrap<WrappedPoly>(info.This());
+
+    v8::String::Utf8Value s(property);
+    std::string str(*s);
+
+    if (str == "a")
+    {
+        info.GetReturnValue().Set(v8::Number::New(isolate, poly->a_));
+    }
+    else if (str == "b")
+    {
+        info.GetReturnValue().Set(v8::Number::New(isolate, poly->b_));
+    }
+    else if (str == "c")
+    {
+        info.GetReturnValue().Set(v8::Number::New(isolate, poly->c_));
+    }
+}
+
+void WrappedPoly::SetCoeff(v8::Local<v8::String> property, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
+{
+    v8::Isolate* isolate = info.GetIsolate();
+    WrappedPoly* poly = ObjectWrap::Unwrap<WrappedPoly>(info.This());
+
+    v8::String::Utf8Value s(property);
+    std::string str(*s);
+
+    if (str == "a")
+    {
+        poly->a_ = value->NumberValue();
+    }
+    else if (str == "b")
+    {
+        poly->b_ = value->NumberValue();
+    }
+    else if (str == "c")
+    {
+        poly->c_ = value->NumberValue();
     }
 }
 
